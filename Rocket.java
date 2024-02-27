7a7e594cab7689c722f32c11020a7e951b7c277a
mrasamny <mrasamny@desu.edu>
2024-02-26 20:23:16 -0500

    first commit

---
+++ Rocket.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rocket here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rocket extends Actor
+{
+    private int speed;
+    
+    public Rocket(){
+        speed = 5;
+    }
+    
+    /**
+     * Act - do whatever the Rocket wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if (mouse != null){
+            turnTowards(mouse.getX(),mouse.getY());
+        }
+        move(speed);
+    }    
+}
